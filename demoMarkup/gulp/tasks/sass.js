30f5cefa14389a15c9377a3b28847d20e2c438cd
Piotr <piotr1_krk@interia.pl>
2016-11-06 17:25:14 +0100

    content + gulp sass improvement

--- demoMarkup/gulp/tasks/sass.js
+++ demoMarkup/gulp/tasks/sass.js
@@ -8,8 +8,14 @@ gulp.task('build-css', function () {
     return gulp.src('src/scss/**/main.scss')
         .pipe(sourcemaps.init())  // Process the original sources
         .pipe(sass())
+        .on('error', swallowError)
         .pipe(sourcemaps.write()) // Add the map to modified source.
         .pipe(autoprefixer('last 2 version'))
         .pipe(gulp.dest('public/assets/css'))
         .pipe(bs.reload({stream: true}));
 });
+
+function swallowError (error) {
+    console.log(error.toString());
+    this.emit('end');
+}
