009c7d4938eef18ac499b4659d0dd1fe24c4e770
Michał Bardzki <m.bardzki@getprintbox.com>
2016-11-06 14:04:21 +0100

    gulp autoprefixer added

--- demoMarkup/gulp/tasks/sass.js
+++ demoMarkup/gulp/tasks/sass.js
@@ -3,5 +3,6 @@ gulp.task('build-css', function () {
         .pipe(sourcemaps.init())  // Process the original sources
         .pipe(sass())
         .pipe(sourcemaps.write()) // Add the map to modified source.
+		.pipe(autoprefixer('last 2 version'))
         .pipe(gulp.dest('public/assets/css'));
 });
