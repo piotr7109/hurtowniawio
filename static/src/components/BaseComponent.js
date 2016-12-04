e37b58bf350a0409d863a07a65bea49694cb7bd9
Piotr <piotr1_krk@interia.pl>
2016-12-04 17:00:30 +0100

    react base component configured

---
+++ static/src/components/BaseComponent.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import axios from 'axios';
+import Error from './error/Error';
+
+export default class BaseComponent extends React.Component {
+
+    constructor() {
+        super();
+        this.setLoggedUser();
+        this.state = {loggedUser: 'undefined'};
+        this.allowedUsers = [];
+    }
+
+    isUserAuthenticated() {
+        return this.allowedUsers.indexOf(this.state.loggedUser) > -1;
+    }
+
+    setLoggedUser() {
+        return axios({
+            method: 'post',
+            url: '/getLoggedUser'
+        }).then(() => {
+            this.setState({loggedUser: 'rolnik'});
+            console.log(this.state.loggedUser);
+        });
+    }
+
+    renderHTML() {
+        return (
+            <div>This is main base component</div>
+        );
+    }
+
+    isComponentReady() {
+        return this.state.loggedUser != 'undefined';
+    }
+
+    render() {
+        if (this.isComponentReady()) {
+            if (this.isUserAuthenticated()) {
+                return this.renderHTML();
+            } else {
+                return Error.getErrorMessage();
+            }
+        } else {
+            return null;
+        }
+
+    }
+}
