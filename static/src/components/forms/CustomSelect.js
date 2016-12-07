386dddfd27d09bddc3fbcef4eabcb10354a795ea
Michał Bardzki <m.bardzki@getprintbox.com>
2016-12-07 23:01:59 +0100

    dropdown

---
+++ static/src/components/forms/CustomSelect.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import _ from 'underscore';
+
+export default class CustomSelect extends React.Component {
+
+    // let values = [
+    //     {text: 'ASasA', value: 'asdasd'}
+    // ]
+    constructor() {
+        super();
+
+        this.state = {
+            isExpanded: false
+        };
+    }
+
+
+    getItem(item) {
+        return (
+            <div className="item" onClick={(event) => this.select(event)}>{item.text}</div>
+        );
+    }
+
+    getList() {
+        let outputList = [];
+
+        let selected = this.state.selected ? this.state.selected : this.props.items[0];
+        console.log(selected)
+        let listOfItems = _.without(this.props.items, selected);
+
+
+
+        console.log('aaaaaaa', listOfItems)
+
+        for (let item of listOfItems) {
+            outputList.push(this.getItem(item));
+        }
+        return outputList;
+    }
+
+    toggle() {
+        this.setState({isExpanded: !this.state.isExpanded});
+    }
+
+    select(event) {
+        this.setState({
+            selected: _.find(this.props.items, {text: event.currentTarget.textContent}),
+            isExpanded: !this.state.isExpanded
+        })
+    }
+
+    render() {
+        let cssStyle = this.state.isExpanded ? null : {display: "none"};
+        let list = this.getList();
+        let selected = this.state.selected ? this.state.selected.text : this.props.items[0].text;
+
+        return (
+            <div className="CustomSelectWrapper">
+                <div className="CustomSelect">
+                    <div className="selected" onClick={() => this.toggle()}>{selected}</div>
+                    <div className="list" style={cssStyle}>{list}</div>
+                </div>
+            </div>
+        );
+    }
+}
--- static/src/components/hello/Hello.js
+++ static/src/components/hello/Hello.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router';
-import Menu from '../../template/menu/Menu'
+import Menu from '../../template/menu/Menu';
 import CustomHeader from '../../template/header/Header';
 import BaseComponent from "../BaseComponent";
-import LoginForm from '../authentication/LoginForm'
+import LoginForm from '../authentication/LoginForm';
+import CustomSelect from '../forms/CustomSelect';
 
 export default class Hello extends BaseComponent {
     constructor() {
@@ -11,9 +12,17 @@ export default class Hello extends BaseComponent {
     }
 
     renderHTML() {
+        let values = [
+            {text: 'pierwszy', value: 'aaaa'},
+            {text: 'drugi', value: 'bbbb'},
+            {text: 'trzeci', value: 'cccc'},
+            {text: 'czwarty', value: 'dddd'},
+            {text: 'piaty', value: 'eeee'}
+        ];
+
         return (
             <div className="hello">
-
+                <CustomSelect items={values} />
                 <LoginForm />
             </div>
         );
