434866510a59e6dc982c63d43ee13404c2b09402
Piotr <piotr1_krk@interia.pl>
2016-12-10 02:51:11 +0100

    gulp code clean up

--- static/gulp/tasks/typescript.js
+++ static/gulp/tasks/typescript.js
@@ -1,26 +1,24 @@
 'use strict';
 
-var gulp = require('gulp'),
+let gulp = require('gulp'),
     typescript = require('typescript'),
     ts = require('gulp-typescript'),
     browserify = require('browserify'),
     source = require('vinyl-source-stream'),
-    del = require('del');
-
-var project = ts.createProject('src/tsconfig.json', {typescript: typescript});
+    del = require('del'),
+    project = ts.createProject('src/tsconfig.json', {typescript: typescript});
 
 gulp.task('typescript-compile', function () {
-    var tsResult = gulp.src("src/**/*.{ts,tsx}") // or tsProject.src()
+    let tsResult = gulp.src("src/**/*.{ts,tsx}") // or tsProject.src()
         .pipe(project());
     return tsResult.js.pipe(gulp.dest('tmpDir'));
 });
 
 gulp.task('typescript-bundle', ['typescript-compile'], function () {
-    var b = browserify('tmpDir/app.js');
+    let b = browserify('tmpDir/app.js');
     return b.bundle()
         .pipe(source('bundle.js'))
-        .pipe(gulp.dest('public/assets/js'))
-        ;
+        .pipe(gulp.dest('public/assets/js'));
 });
 
 gulp.task('clean', function (done) {
