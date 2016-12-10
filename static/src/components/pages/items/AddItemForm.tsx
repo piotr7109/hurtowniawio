5ef59929b732920ee7eb7dcea74385a1fb61ef8d
Piotr <piotr1_krk@interia.pl>
2016-12-10 23:28:31 +0100

    changed files structure

--- static/src/components/pages/items/AddItemForm.tsx
+++ static/src/components/pages/items/AddItemForm.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
-import {BaseForm} from "../BaseForm";
+import {Link} from 'react-router';
+import {BaseForm} from '../BaseForm';
+import {BasicInputControl} from '../../partials/forms/controls/BasicInputControl';
 
 export default class AddItemForm extends BaseForm {
+
+    formControls = [
+        {name: 'name', text: 'Nazwa', type: 'text'},
+        {name: 'country', text: 'Kraj pochodzenia', type: 'text'},
+        {name: 'typeName', text: 'Odmiana', type: 'text'}
+    ];
 
     allowedUsers = [
         this.userTypes.hurtownik
@@ -15,17 +23,34 @@ export default class AddItemForm extends BaseForm {
         return (
             <form className="AddItemForm navbar-form" onSubmit={this.handleSubmit.bind(this)}>
                 <div className="form-group">
+                    {this.formControls.map((item) => {
+                        return (<BasicInputControl
+                            name={item.name}
+                            type={item.type}
+                            text={item.text}
+                            key={item.name}
+                            value=""/>);
+                    })}
                 </div>
             </form>
         );
     }
 
     getSuccessMessage(): any {
-        return undefined;
+        return (
+            <div>
+                Przedmiot został dodany do bazy przedmiotów
+                <Link to="/">Powrót na stronę główną</Link>
+            </div>
+        );
     }
 
     getErrorMessage(): any {
-        return undefined;
+        return (
+            <div>
+                Błąd, ale nie wiem ;____;
+            </div>
+        );
     }
 
 }
