49f52c32da14e98f27b1063d1336c067a0076c5a
Piotr <piotr1_krk@interia.pl>
2016-12-30 01:10:28 +0100

    error 404 styling

--- static/src/components/pages/error/Errors.tsx
+++ static/src/components/pages/error/Errors.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import {Link} from 'react-router';
 
 export default class Error404 extends React.Component<void, void> {
     render() {
         return (
-            <div>ERROR 404</div>
+            <div className="Error404">
+                <div className="error-code">404</div>
+                <div className="error-message">
+                    Strona nie istnieje, zapraszamy na <Link to="/">stronę główną!</Link>
+                </div>
+            </div>
         );
     }
 }
