e21aa6abc55b83e715e5c41e6486baa02cadd6bc
Piotr <piotr1_krk@interia.pl>
2017-01-02 18:20:44 +0100

    refactoring

--- static/src/components/pages/users/userProfile/UserProfile.tsx
+++ static/src/components/pages/users/userProfile/UserProfile.tsx
@@ -17,7 +17,8 @@ export default class UserProfile extends BasePage<Props, BaseStates> {
     }
 
     loadData() {
-        let userId = this.props.params ? this.props.params.id : this.props.userId;
+        let params = this.props.params,
+            userId = params ? params.id : this.props.userId;
 
         if (UserUtils.loggedUser.type === UserUtils.userTypes.rolnik || !userId) {
             this.user = UserUtils.loggedUser;
