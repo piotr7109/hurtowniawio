6694a5d2e737b64cc40faa1a0df3a7b89d032f0d
Piotr <piotr1_krk@interia.pl>
2017-01-02 18:53:02 +0100

    build and run batch file

--- src/main/java/com/beef/core/bootstrap/DatabaseCreation.java
+++ src/main/java/com/beef/core/bootstrap/DatabaseCreation.java
@@ -1,5 +1,7 @@
 package com.beef.core.bootstrap;
 
+import com.beef.domian.user.User;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -9,7 +11,26 @@ public class DatabaseCreation {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hurtowniawioDatabaseCreate");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
+        entityManager.getTransaction().begin();
+        entityManager.persist(getDefaultAdminUser());
+        entityManager.getTransaction().commit();
+
         entityManager.close();
         entityManagerFactory.close();
     }
+
+    private static User getDefaultAdminUser() {
+        User user = new User();
+        user.setLogin("admin");
+        user.setPassword("admin");
+        user.setFirstName("admin");
+        user.setLastName("admin");
+        user.setAddress("admin address");
+        user.setEmail("admin@admin.pl");
+        user.setStatus("A");
+        user.setType("admin");
+
+        return user;
+    }
 }
+
